6ebd022f33c34021532187b64bd4463e27468e56
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2025-06-24 09:34:43 -0300

    feat: add comentario

--- src/modules/Auth/controllers/AuthController.ts
+++ src/modules/Auth/controllers/AuthController.ts
@@ -4,14 +4,16 @@ import { UserRepository } from "@db/repositories/UserRepository";
 import { CreateAuthService } from "../services/CreateAuthService";
 import { GetAuthUserService } from "../services/GetAuthUserService";
 
+// Comentário do curso Git.
+
 class UserController {
   async create(req: Request, res: Response) {
-    const service = new CreateAuthService(new UserRepository());
-    const { login, password } = req.body;
+    // const service = new CreateAuthService(new UserRepository());
+    // const { login, password } = req.body;
 
-    const result = await service.execute(login, password);
+    // const result = await service.execute(login, password);
 
-    res.json(result);
+    res.json({ joinha: "true" });
   }
 
   async index(req: Request, res: Response) {
