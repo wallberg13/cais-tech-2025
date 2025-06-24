70e82b99de43df0ac13828ae4962d0ebfcd62031
Mathyas <mathyasmartins25@gmail.com>
2025-06-24 09:32:53 -0300

    feat: add comentario

--- src/modules/Auth/controllers/AuthController.ts
+++ src/modules/Auth/controllers/AuthController.ts
@@ -3,14 +3,16 @@ import { Request, Response } from "express";
 import { UserRepository } from "@db/repositories/UserRepository";
 import { CreateAuthService } from "../services/CreateAuthService";
 import { GetAuthUserService } from "../services/GetAuthUserService";
-
+// comentário do curso de git
+// isso é um teste
+//seu paig eh gay
 class UserController {
   async create(req: Request, res: Response) {
     const service = new CreateAuthService(new UserRepository());
     const { login, password } = req.body;
 
     const result = await service.execute(login, password);
-
+// quem ler é gay
     res.json(result);
   }
 
